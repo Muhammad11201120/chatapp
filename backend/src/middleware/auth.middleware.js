8a9966407a281fbad8c8a3b38d72f5954fa31103
Muhammed <muha.11201120@gmail.com>
2025-09-17 12:48:51 +0300

    update profile implemnted

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+export const protectRoute = async (req, res, next) => {
+  try {
+    // Get token from cookies
+    const token = req.cookies?.token;
+    // Check if token exists
+    if (!token)
+      return res
+        .status(401)
+        .json({ message: "غير مصرح، يرجى تسجيل الدخول!" });
+    const decoded = jwt.verify(token, ENV.JWT_SECRET);
+    // Check if token is valid
+    if (!decoded)
+      return res.status(401).json({ message: "غير مصرح، يرجى تسجيل الدخول!" });
+    // Optionally, you can check if the user still exists in the database
+    // Our JWT payload is { id: userId }, so use decoded.id
+    // Also ensure we await the DB call
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user)
+      return res.status(401).json({ message: "غير مصرح، يرجى تسجيل الدخول!" });
+    // Attach user information to request object
+    req.user = user;
+    // Proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    console.error("Error in protectRoute middleware:", error);
+    return res
+      .status(500)
+      .json({ message: "خطأ في الخادم يرجى المحاولة لاحقا !" });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,19 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 const app = express();
 const PORT = ENV.PORT || 3000;
 // Serve static files from the React frontend app
 const __dirname = path.resolve();
 
 app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+// Serve static files in production
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
