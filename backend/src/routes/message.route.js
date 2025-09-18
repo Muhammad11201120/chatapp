3680bb7a00441569afbac92f29fe09a94769253f
Muhammed <muha.11201120@gmail.com>
2025-09-18 16:47:35 +0300

    messages controller and end pointes created

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetMiddleware } from "../middleware/arcjet.middleware.js";
+
 const router = express.Router();
-router.get('/send', (req, res) => {
-    res.send('send message endpoint');
-});
-router.get('/inbox', (req, res) => {
-    res.send('inbox endpoint');
-});
+// Apply arcjet middleware and protectRoute to all routes
+// All middlewares executed in order so requests is rate limited first then authenticated
+// this is actually more efficient since authontcated requests get blocked if they are rate limited
+// before hitting the auth middleware
+router.use(arcjetMiddleware, protectRoute);
+// Apply arcjet middleware to all routes
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
