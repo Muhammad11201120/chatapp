810cbdb623d3ddd78170a0283c17e81c2f1a8c5b
Muhammed <muha.11201120@gmail.com>
2025-09-19 13:16:59 +0300

    set up Chats Page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,33 @@
-import { useAuthStore } from "../store/useAuthStore";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import { useChatStore } from "../store/useChatStore";
+import ProfileHeader from "../components/ProfileHeader";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatsList from "../components/ChatsList";
+import ContactsList from "../components/ContactsList";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceHolder from "../components/NoConversationPlaceHolder";
 const ChatPage = () => {
-  const { logout } = useAuthStore();
+  const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <button onClick={logout} className="z-10 border  p-2 cursor-pointer">
-      تسجيل الخروج
-    </button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* RIGHT SIDE */}
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactsList />}
+          </div>
+        </div>
+
+        {/* LEFT SIDE */}
+        <div className="flex-1 flex flex-col bg-slae-800/50 backdrop-blur-sm">
+          {selectedUser ? <ChatContainer /> : <NoConversationPlaceHolder />}
+        </div>
+      </BorderAnimatedContainer>
+    </div>
   );
 };
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,8 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+
 export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
