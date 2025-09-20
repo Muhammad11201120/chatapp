4fd49b6e1cdb74be8394b34a4a6115119cf3b5bf
Muhammed <muha.11201120@gmail.com>
2025-09-20 15:28:50 +0300

    changing some server.js functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-import cookieParser from "cookie-parser";
+
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
